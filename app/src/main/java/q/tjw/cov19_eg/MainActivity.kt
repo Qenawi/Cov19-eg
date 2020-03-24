95fd8f680058a662ed71df3fc47530a022ad06d7
qenawiTJW <aldmanmero@yahoo.com>
2020-03-24 18:46:48 -0400

    Initial commit

---
+++ app/src/main/java/q/tjw/cov19_eg/MainActivity.kt
@@ -0,0 +1,10 @@
+package q.tjw.cov19_eg
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
