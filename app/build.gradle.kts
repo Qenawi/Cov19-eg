fb95571f08cb83acd5ad0988d8dd5660b6690e29
qenawiTJW <aldmanmero@yahoo.com>
2020-03-25 11:48:14 -0400

    updated gradle to last versiom according to Tivi Repo

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,5 +27,6 @@ dependencies {
     implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
     implementation(Libraries.Libs.kotlin)
     implementation(Libraries.Libs.appcompat)
+    implementation(Libraries.constraintlayout)
 }
 
